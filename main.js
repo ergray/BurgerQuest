4e5d375599e5c3035491e221a167356be162960a
Eric Gray <ericxgray@gmail.com>
2017-02-10 14:57:19 -0800

    structural changes

--- main.js
+++ main.js
@@ -1,64 +1,19 @@
 window.onload = function(){
-	var thisGame = Game;
-	var foreground = document.getElementById("foreground");
-	var forContext = foreground.getContext("2d");
-	var background = document.getElementById("background");
-	var backContext = background.getContext("2d");
-
-	var brick = new Image();
-	brick.src = ("./assets/brick.png");
-
-	brick.onload = function(){
-		var xCur = 0;
-		var yCur = 0;
-		_.each(Game.map, function(row){
-			_.each(row, function(cell){
-				//if cell has a 1, place a wall
-				if (cell==1){
-					backContext.drawImage(brick, xCur, yCur);
-					xCur+=50;
-				//0 is walkable terrain	
-				// } else if (cell==0){
-				// 	xCur+=50;
-				// }
-				} else {
-					xCur+=50;
-				}				
-				if (xCur == 500){
-					xCur=0;
-					yCur+=50;
-				}
-			})
-		})
-	}
-
-	var worker = new Image();
-	worker.src = ("./assets/hero.png");
-	worker.onload = function(){
-		forContext.drawImage(worker, Game.hero.xPOS, Game.hero.yPOS);
-	}
-
-	var money = new Image();
-	money.src = ("./assets/money.png");
-	money.onload=function(){
-		forContext.drawImage(money, 250, 200);
-	}
-
+	thisGame = new Game();
+	var context = new Context();
+	thisGame.context = context;
 
 	window.addEventListener('keydown', function(e){
-		if (Game.inMenu == false){
+		if (thisGame.inMenu == false){
 			if (e.keyCode == 87 || e.keyCode == 83 || e.keyCode == 65 || e.keyCode == 68){
-				Game.repos(e, worker)
+				thisGame.repos(e, context.worker)
 			} else if (e.keyCode == 32){
-				Game.interact(e);
-			}} else if (Game.inMenu == true){
+				thisGame.interact(e, context.menu, context.menuContext);
+			}} else if (thisGame.inMenu == true){
 				if (e.keyCode == 27){
-					Game.exitMenu();
+					thisGame.exitMenu();
 				}
 			}
-
-
-
 		}, true)
 
 }
